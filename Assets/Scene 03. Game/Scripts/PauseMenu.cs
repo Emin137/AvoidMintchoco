0b0544f27317ff5973de760cfc3a1461505d7f35
uhzoo <tjdnwn19@gmail.com>
2022-08-15 13:45:38 +0900

    220815 13:45
    pause, resume, loby

---
+++ Assets/Scene 03. Game/Scripts/PauseMenu.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] Button pauseButton;
+    [SerializeField] Button resumeButton;
+    [SerializeField] Button LobyButton;
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenu;
+
+    // Update is called once per frame
+    private void Awake()
+    {
+        pauseButton.onClick.AddListener(Pause);
+        resumeButton.onClick.AddListener(Resume);
+        LobyButton.onClick.AddListener(Loby);
+    }
+
+    private void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    private void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    private void Loby()
+    {
+        SceneManager.LoadScene("Scene02. Loby");
+    }
+}
