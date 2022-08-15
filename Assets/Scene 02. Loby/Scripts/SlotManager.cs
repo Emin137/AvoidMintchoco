c1848dce064d14788c3adec9120479a354007cf2
Emin <emin98090@naver.com>
2022-08-15 10:58:42 +0900

    220815 10:58

--- Assets/Scene 02. Loby/Scripts/SlotManager.cs
+++ Assets/Scene 02. Loby/Scripts/SlotManager.cs
@@ -7,34 +7,45 @@ using UnityEngine.SceneManagement;
 public class SlotManager : MonoBehaviour
 {
     public GameObject slotObject;
-    public Button slot;
     public Image[] images;
+    public Sprite[] sprites;
     public Text text;
     public int rulletNum;
-
-    private void Awake()
-    {
-        slot.onClick.AddListener(SceneTrans);
-    }
+    List<string> list = new List<string>();
 
     private void Start()
     {
-        int N = Random.Range(1, 3);
-        if(N==1)
+        int N = Random.Range(0, 3);
+        switch (N)
         {
-
-        }   
-        else if(N==2)
-        {
-
+            case 0: // 경험치 획득량 증가
+                images[0].sprite = sprites[0];
+                images[1].sprite = sprites[1];
+                images[2].sprite = sprites[2];
+                images[3].sprite = sprites[0];
+                list.Add("경험치 획득량 증가");
+                list.Add("생명력 증가");
+                list.Add("이동속도 증가");
+                break;
+            case 1: // 생명력 증가
+                images[0].sprite = sprites[1];
+                images[1].sprite = sprites[2];
+                images[2].sprite = sprites[0];
+                images[3].sprite = sprites[1];
+                list.Add("생명력 증가");
+                list.Add("이동속도 증가");
+                list.Add("경험치 획득량 증가");
+                break;
+            case 2: // 이동속도 증가
+                images[0].sprite = sprites[2];
+                images[1].sprite = sprites[0];
+                images[2].sprite = sprites[1];
+                images[3].sprite = sprites[2];
+                list.Add("이동속도 증가");
+                list.Add("경험치 획득량 증가");
+                list.Add("생명력 증가");
+                break;
         }
-        else
-        {
-
-        }
-        
-       
-        slot.interactable = false;
         StartCoroutine(StartSlot());
     }
 
@@ -52,28 +63,21 @@ public class SlotManager : MonoBehaviour
             switch (count)
             {
                 case 1:
-                    text.text = images[2].ToString();
+                    text.text = list[0];
                     break;
                 case 2:
-                    text.text = images[0].ToString();
+                    text.text = list[1];
                     break;
                 case 4:
-                    text.text = images[1].ToString();
+                    text.text = list[2];
                     break;
                 case 6:
-                    text.text = images[2].ToString();
+                    text.text = list[0];
                     count = 0;
                     break;
             }
 
             yield return new WaitForSeconds(0.02f);
         }
-        slot.interactable = true;
     }
-
-    private void SceneTrans()
-    {
-        SceneManager.LoadScene("Scene03. Game");
-    }
-
 }
--- Assets/Scene 02. Loby/Scripts/UIManager.cs
+++ Assets/Scene 02. Loby/Scripts/UIManager.cs
@@ -1,22 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField] private Button gameReadyButton;
     [SerializeField] private Button gameStartButton;
     [SerializeField] private GameObject panelGameObject;
     [SerializeField] private GameObject panelLoby;
 
     private void Awake()
     {
-        gameStartButton.onClick.AddListener(SetActivePanel);
+        gameReadyButton.onClick.AddListener(SetActivePanel);
+        gameStartButton.onClick.AddListener(SceneTrans);
     }
 
     private void SetActivePanel()
     {
         panelGameObject.SetActive(true);
         panelLoby.SetActive(false);
+    }
+
+    private void SceneTrans()
+    {
+        SceneManager.LoadScene("Scene03. Game");
     }
 }
